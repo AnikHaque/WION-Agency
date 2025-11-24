474b1df47a4fafb88776bb5a0c613ce5f0f303cd
AnikHaque <ekramul15-811@diu.edu.bd>
2025-11-25 02:27:13 +0600

    CTA done

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,6 +1,6 @@
 export function CTA() {
 return (
-<section className="py-20 bg-blue-600 text-white text-center">
+<section className="py-20 bg-gray-50 text-black text-center">
 <h2 className="text-4xl font-bold mb-6">Ready to Work With Us?</h2>
 <p className="text-lg mb-6">Let’s build something amazing together.</p>
 <button className="px-8 py-3 bg-white text-blue-600 rounded-lg text-lg font-semibold hover:bg-gray-100 transition">
