64094c0cc9be27e5958ceaff84d2f627c553b819
AnikHaque <ekramul15-811@diu.edu.bd>
2025-11-27 01:33:00 +0600

    contact done

---
+++ src/components/ContactBanner.jsx
@@ -0,0 +1,29 @@
+import ContactBreadcrumb from "../utils/ContactBredcrumb";
+
+
+
+const ContactBanner = () => {
+  return (
+     <div
+          className="w-full h-[50vh] bg-cover bg-center relative flex items-center justify-center"
+          style={{
+            backgroundImage:
+              "url('https://wp.framerpeak.com/wion/wp-content/uploads/2025/10/inner-ab2-thumb10.png')",
+          }}
+        >
+          {/* WRAPPER TO STACK HEADING + BREADCRUMB */}
+          <div className="flex flex-col items-center space-y-4">
+            <h1 className="relative text-white text-4xl md:text-5xl font-semibold bg-gray-500 p-4">
+              Contact Us
+            </h1>
+    
+            {/* Breadcrumb under heading */}
+            <div className=" bg-opacity-40 px-4 py-1 rounded-md">
+             <ContactBreadcrumb></ContactBreadcrumb>
+            </div>
+          </div>
+        </div>
+  );
+};
+
+export default ContactBanner;
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,8 +1,10 @@
 import ContactSection from "../components/Contact";
+import ContactBanner from "../components/ContactBanner";
 
 const ContactPage = () => {
   return (
     <>
+    <ContactBanner></ContactBanner>
    <ContactSection></ContactSection>
     
     </>
