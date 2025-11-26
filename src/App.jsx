2ce6daed7c6699e4fcb00a602a6d2380158ff99a
AnikHaque <ekramul15-811@diu.edu.bd>
2025-11-27 01:27:46 +0600

    contact  added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import FeatureDetails from "./components/FeatureDetails";
 import AboutPage from "./pages/About";
 import Layout from "./Layout/MainLayout";
 import Portfolio from "./pages/Portfolio";
+import ContactPage from "./pages/ContactPage";
 
 
 export default function App() {
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<ContactPage />} />
         <Route path="/features/:id" element={<FeatureDetails />} />
       </Route>
     </Routes>
---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,12 @@
+import ContactSection from "../components/Contact";
+
+const ContactPage = () => {
+  return (
+    <>
+   <ContactSection></ContactSection>
+    
+    </>
+  );
+};
+
+export default ContactPage;
